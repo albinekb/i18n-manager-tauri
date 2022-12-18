30ca29968c09f63cacc983eeb0195df7d0da64bc
Albin Ekblom <albinekblom@gmail.com>
2022-12-18 16:26:38 +0100

    Check version

--- scripts/sync-release-branch.sh
+++ scripts/sync-release-branch.sh
@@ -2,6 +2,9 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+bold=$(tput bold)
+normal=$(tput sgr0)
+
 if [[ -n $(git status --short) ]]; then
   echo "Git working tree is dirty, exiting.."
   exit 0
@@ -12,9 +15,25 @@ if [[ $(git branch --show-current) != "development" ]]; then
   git checkout development
 fi
 
-echo "Syncing release branch with development.."
+echo "Fetching latest changes.."
 git pull origin development
 git checkout release
+git pull origin release
+
+echo "Checking versions..."
+RELEASED_VERSION=$(git show release:package.json | jq -r '.version')
+CURRENT_VERSION=$(cat package.json | jq -r '.version')
+
+echo "${bold}release version${normal}: $RELEASED_VERSION
+${bold}development version${normal}: $CURRENT_VERSION"
+
+if [[ $RELEASED_VERSION == $CURRENT_VERSION ]]; then
+  echo "Current version is already released, exiting.."
+  git checkout development
+  exit 0
+fi
+
+echo "${bold}Merging development into release..${normal}"
 git merge development
 git push origin release
 git checkout development
