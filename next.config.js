22c94a8df0016af6400076bb6dacb111aa9d10c8
Albin Ekblom <albinekblom@gmail.com>
2022-12-19 23:35:03 +0100

    🌹 Transform material imports

--- next.config.js
+++ next.config.js
@@ -6,6 +6,25 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    modularizeImports: {
+      lodash: {
+        transform: 'lodash/{{member}}',
+      },
+      '@mui/material': {
+        transform: '@mui/material/{{member}}',
+      },
+      '@mui/icons-material': {
+        transform: '@mui/icons-material/{{member}}',
+      },
+      '@mui/system': {
+        transform: '@mui/system/{{member}}',
+      },
+      '@mui/lab': {
+        transform: '@mui/lab/{{member}}',
+      },
+    },
+  },
 }
 
 module.exports = nextConfig
