be220e165332eeef966458d32f1a9954dcdae11f
Albin Ekblom <albinekblom@gmail.com>
2022-12-19 16:48:32 +0100

    🌹 Fix changelog in CI

--- scripts/get-changelog.sh
+++ scripts/get-changelog.sh
@@ -1,25 +1,27 @@
 
 #!/bin/bash
-set -euo pipefail
+if [[ ! -z ${CI} ]]; then
+  set -euo pipefail
+fi
 IFS=$'\n\t'
-
+export PAGER="cat"
 if [[ $(git branch --show-current) == "development" ]]; then
   log=$(git log --graph --pretty=format:'%Cred%h%Creset  %s%Creset' --invert-grep --grep="🚢" release..development)
   if [[ -z ${log} ]]; then
     echo "No log found, exiting.."
-    exit 1
+    exit 0
   fi
   echo "${log}"
 else
-  log = $(git log -1 --pretty=%B)
+  log=$(git log -1 --pretty=%B)
   if [[ -z "${log}" ]]; then
     echo "No log found, exiting.."
-    exit 1
+    exit 0
   fi
   if [[ ! "$log" == *"🚢"* ]]; then
     echo "No release found, exiting.."
-    exit 1
+    exit 0
   fi
    
-  echo -e "${log}" | tail -n +3 | sed -e '$ d'
+  echo -e "${log}" | tail -n +2 | grep -Ev "^\$"
 fi
