ac11c6c09ff46f66ad087aec36715343b2651540
Albin Ekblom <albinekblom@gmail.com>
2022-12-20 14:54:29 +0100

    🌹 Fix get-changelog

--- scripts/get-changelog.sh
+++ scripts/get-changelog.sh
@@ -7,15 +7,10 @@ IFS=$'\n\t'
 export PAGER="cat"
 
 last_tag=$(git tag --sort=creatordate | grep -A 1 ^v | tail -n 1)
-log=$(git for-each-ref --format="%(body)" refs/tags/${last_tag} | grep -Ev "^\$")
+log=$(git tag -l --format="%(contents)" ${last_tag} | grep -Ev "^\$")
 if [[ -z "${log}" ]]; then
   echo "No log found, exiting.."
   exit 0
 fi
-subject=$(git for-each-ref --format="%(subject)" refs/tags/${last_tag})
-if [[ ! "$subject" == *"🚢"* ]]; then
-  echo "No release found, exiting.."
-  exit 0
-fi
-  
+
 echo "${log}"
